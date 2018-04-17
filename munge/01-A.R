63b73d1d198e2a00048a6e646b084d9734a1df12
hakanegeli <hakanegeli@gmail.com>
2018-04-16 18:18:25 -0700

    First model, ver 1

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,2 +1,11 @@
-# Example preprocessing script.
-customer.count <- ddply(CustomerData, c('CustomerId'), nrow)
+# Fix the seed to have the same random number sequence for subsequent runs
+set.seed(415)
+
+# CustomerData should be auto loaded so select the features/independent variables from the Customerdata
+dataset <- CustomerData[,2:21]
+
+# split the dataset into training and test sets
+split = sample.split(dataset$CreditLimit, SplitRatio = 0.7)
+
+dataset_train = subset(dataset, split == TRUE) 
+dataset_test = subset(dataset, split == FALSE)
