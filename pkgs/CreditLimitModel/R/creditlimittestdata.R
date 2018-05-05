b775e4b4a0caf8efa62e677f2e465564eed8686d
hakanegeli <hakanegeli@gmail.com>
2018-05-05 00:21:31 -0700

    updated CreditLimitModel package, added test data

---
+++ pkgs/CreditLimitModel/R/creditlimittestdata.R
@@ -0,0 +1,39 @@
+#' Credit Limit Test Case
+#'
+#' Dataset containing 340 test cases
+#'
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{CreditLimit}{Credit limit}
+#'  \item{CreditLimitLocked}{Indicator whether the credit limit is locked for prevent updates}
+#'  \item{JBTRating}{Credit rating provided by JBT}
+#'  \item{SalesCurrentYr}{Net sales for the current year}
+#'  \item{SalesLastYr}{Net sales for the last year}
+#'  \item{PlainBandSalesLast12Mo}{Net plain band sales for the last 12 months}
+#'  \item{DesignBandSalesLast12Mo}{Net design band sales for the last 12 months}
+#'  \item{DiamondBandSalesLast12Mo}{Net diamond band sales for the last 12 months}
+#'  \item{AlternativeMetalSalesLast12Mo}{Net alternative metal band sales for the last 12 months}
+#'  \item{TotalSalesLast12Mo}{Net total sales for the last 12 months}
+#'  \item{BuyingGroupMember}{}
+#'  \item{NumberOfStoreLocations}{}
+#'  \item{AvgDaysOfPayCategory}{}
+#'  \item{AvgDaysOfPay}{}
+#'  \item{MaxDaysPastDue}{}
+#'  \item{MaxAmtPastDue}{}
+#'  \item{AvgAmtPastDue}{}
+#'  \item{ReturnedPaymentCount}{}
+#'  \item{MaxReturnedPaymentAmt}{}
+#'  \item{RuralUrbanContinuumCode}{}
+#'  \item{UrbanInfluenceCode}{}
+#'  \item{MetroIndicator}{}
+#'  \item{TotalPopulation}{}
+#'  \item{MedianEarnings}{}
+#'  \item{MedianRetailTradeEarnings}{}
+#'  \item{TotalHouseholds}{}
+#'  \item{MedianIncomeHouseholds}{}
+#'  \item{NumberOfJewelryStores}{}
+#'  \item{NumberOfJewelryStoresState}{}
+#'  \item{JewelryStoreSalesState}{}
+#'  \item{CreditLimitCategory}{Credit limit category}
+#' }
+"dataset_test"
