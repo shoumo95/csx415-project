f185958eed8ab4f883d4ebdc8fb604d9e9b9ab7f
hakanegeli <hakanegeli@gmail.com>
2018-05-06 16:53:18 -0700

    package correction, v0.9.9.0001

--- pkgs/CreditLimitModel/R/getCreditModel.R
+++ pkgs/CreditLimitModel/R/getCreditModel.R
@@ -3,6 +3,7 @@
 #' This function returns the most recent trained model for determining customer credit limits.
 #' @param none method does not require an input
 #' @return This method will return a trainned model to be used in the predict() fucntion
+#' @export
 #' @examples
 #'
 #' data("creditlimittestdata")
