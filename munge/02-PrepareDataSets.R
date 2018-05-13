8862229070cb64eb32fc3c29f617b714ab9fa9cd
hakanegeli <hakanegeli@gmail.com>
2018-05-13 13:40:07 -0700

    folder cleanup, added comments to the data munge and slipt the file into two

---
+++ munge/02-PrepareDataSets.R
@@ -0,0 +1,15 @@
+# Fix the seed to have the same random number sequence for subsequent runs
+set.seed(415)
+
+# CustomerData should be auto loaded so select the features/independent variables from the Customerdata
+dataset <- CustomerData[,9:39]
+
+# create categorical values for the credit limit for logistic regression models
+dataset$CreditLimitCategory <- factor(dataset$CreditLimit)
+levels(dataset$CreditLimitCategory) <- make.names(levels(factor(dataset$CreditLimit)))
+
+# split the dataset into training and test sets
+trainIndex <- createDataPartition(dataset$CreditLimitCategory, p=0.80, list=FALSE)
+
+dataset_train = dataset[trainIndex,]
+dataset_test = dataset[-trainIndex,]
