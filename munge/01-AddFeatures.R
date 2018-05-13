d030ad69f1d19d3d6b414a2c2b723d3c9e0096b5
hakanegeli <hakanegeli@gmail.com>
2018-05-13 13:40:07 -0700

    folder cleanup, added comments to the data munge and slipt the file into two

--- munge/01-AddFeatures.R
+++ munge/01-AddFeatures.R
@@ -1,34 +1,42 @@
-# Fix the seed to have the same random number sequence for subsequent runs
-set.seed(415)
-
-
+# dataset ECN.2012.US.44A1.with.ann (Census data for Retail Trade: Geographic Area Series: Summary Statistics for the U.S., States, Metro Areas, Counties, and Places) 
+# contains the annual sales information (RCPTOT) for various NAICS. NAICS code 448310 is specific to Jewelry Stores
 data <- ECN.2012.US.44A1.with.ann
 data <- data[data$NAICS.id=="448310",]
 
+# RCPTOT values for some of the statistical areas are masked with the letter D for privacy reasons. We need to take care of these missing values
+# These values are automatically loaded as factors. We need to convert them to numbers and assign NAs for the "D" values (as.numeric automatically assigns NAs)
 data <- data.frame(lapply(data, as.character), stringsAsFactors=FALSE)
 data$RCPTOT = as.numeric(data$RCPTOT)
 
+# missing store sales values can be replaced by an average for this field. However, average within each Urban Influence Code would be a better substitution for the missing values
+# using the CBSACodeUrbanInfluenceCode dataset which translates CBSACodes to Urban Influence Codes we can now take advantage of the Urban Influence Code to determine our means
 data <- merge(data, CBSACodeUrbanInfluenceCode, by.x = "GEO.id2", by.y = "CBSACode", all.x = TRUE)
 
-
+# calculate the means for each of the Urban Influence Codes
 UrbanInfluenceCode_means <- data %>%
   group_by(UrbanInfluenceCode) %>%
   summarize(mean_RCPTOT = mean(RCPTOT, na.rm = TRUE))
 
+# however, there are some urban influence codes which has no data at all. These are primarily non-urban areas. We will determine a non-urban mean and assign this mean to the other missing urban influence codes
 missing_mean <- mean(data$RCPTOT[data$UrbanInfluenceCode>2], na.rm = T)
 
+# prepare a full list of Urban Influence codes and means for these codes
 UrbanInfluenceCode_means <- CustomerData %>% select(UrbanInfluenceCode) %>% distinct() %>% arrange(UrbanInfluenceCode) %>%
   left_join(UrbanInfluenceCode_means, by="UrbanInfluenceCode", all.x = TRUE)
 
+# and also set the missing_mean to the urband influence codes with NA values
 UrbanInfluenceCode_means$mean_RCPTOT[is.na(UrbanInfluenceCode_means$mean_RCPTOT)] <- missing_mean
 
+# add the RCPTOT field to the CustomerData dataset using a left join
 merged <- merge(CustomerData, data[,c("GEO.id2", "RCPTOT")], by.x = "CBSACode", by.y = "GEO.id2", all.x = TRUE)
 
+# and bring the UrbanInfluenceCode_means to the merged dataset so that we can use these means to substitute the missing RCPTOT
 merged <- merge(merged, UrbanInfluenceCode_means, by = "UrbanInfluenceCode", all.x = TRUE)
 
+# if RCPTOT is NA use the UrbanInfluenceCode_means, else keep it
 merged$RCPTOT = ifelse(is.na(merged$RCPTOT), merged$mean_RCPTOT, merged$RCPTOT)
 
-# we are done with the mean_RCPTOT column
+# we are done with the mean_RCPTOT column, remove it
 merged$mean_RCPTOT <- NULL
 
 column_names <- c("CustomerId",
@@ -71,17 +79,5 @@ column_names <- c("CustomerId",
                   "JewelryStoreSalesState",
                   "RCPTOT")
 
+# assign all the columns plus the new RCPTOT column back to the Customer Data
 CustomerData <- merged[,column_names]
-
-# CustomerData should be auto loaded so select the features/independent variables from the Customerdata
-dataset <- CustomerData[,9:39]
-
-# create categorical values for the credit limit for logistic regression models
-dataset$CreditLimitCategory <- factor(dataset$CreditLimit)
-levels(dataset$CreditLimitCategory) <- make.names(levels(factor(dataset$CreditLimit)))
-
-# split the dataset into training and test sets
-trainIndex <- createDataPartition(dataset$CreditLimitCategory, p=0.80, list=FALSE)
-
-dataset_train = dataset[trainIndex,]
-dataset_test = dataset[-trainIndex,]
